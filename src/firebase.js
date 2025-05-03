5efe98ac0594c358fac15e1c18ad4e5394202ead
Chinthana <chinthanas70@gmail.com>
2025-05-03 23:57:21 +0530

    Fix: remove homepage from package.json for Vercel

--- src/firebase.js
+++ src/firebase.js
@@ -1,29 +1,46 @@
-import { getFirestore } from "firebase/firestore";
-import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// import { getFirestore } from "firebase/firestore";
+// import { initializeApp } from "firebase/app";
+// // import { getAnalytics } from "firebase/analytics";
+// import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCnW1gfMKGd4xeQ6CO11PR_i_MXPlOeElA",
+//   authDomain: "tattoo-20550.firebaseapp.com",
+//   projectId: "tattoo-20550",
+//   storageBucket: "tattoo-20550.firebasestorage.app",
+//   messagingSenderId: "806899523227",
+//   appId: "1:806899523227:web:21bf717598f0b3bc36303e",
+//   measurementId: "G-LM5ZEHRSBM"
+// };
+
+// // Initialize Firebase
+// // const app = initializeApp(firebaseConfig);
+// // const analytics = getAnalytics(app);
+
+// const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app);
+// const provider = new GoogleAuthProvider();
+// const db = getFirestore(app);
+
+// export { auth, provider, signInWithPopup, signOut , db};
+
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCnW1gfMKGd4xeQ6CO11PR_i_MXPlOeElA",
-  authDomain: "tattoo-20550.firebaseapp.com",
-  projectId: "tattoo-20550",
-  storageBucket: "tattoo-20550.firebasestorage.app",
-  messagingSenderId: "806899523227",
-  appId: "1:806899523227:web:21bf717598f0b3bc36303e",
-  measurementId: "G-LM5ZEHRSBM"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
-const db = getFirestore(app);
-
-export { auth, provider, signInWithPopup, signOut , db};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+export const auth = firebaseApp.auth();
+export default firebaseApp;
